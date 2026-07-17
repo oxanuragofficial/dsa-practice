97e21af7508f22bff7968231073ce030c4e1d83a
Anurag Kumar <hindvansianurag@gmail.com>
2026-07-17 10:21:48 +0530

    Time: 3 ms (59.98%), Space: 44.9 MB (42.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+            s = s.toLowerCase(); 
+
+        StringBuilder result = new StringBuilder();
+        boolean ans=true;
+         for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+              if (c >= 'a' && c <= 'z'|| (c >= '0' && c <= '9')) {
+        result.append(c);
+            }
+        }
+         
+        int left = 0;
+        int right = result.length() - 1;
+        
+       
+        while (left < right) {
+            if (result.charAt(left) != result.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--; 
+        }
+        return ans;
+    }
+}
