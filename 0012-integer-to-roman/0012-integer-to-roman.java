a76f033fafa8b07f7ca39ed0850f2787b5dfd1d0
Anurag Kumar <hindvansianurag@gmail.com>
2026-07-25 19:42:55 +0530

    Time: 3 ms (99.87%), Space: 46.3 MB (61.15%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,17 +1,5 @@
 class Solution {
-    public static String integerof(int n){
-        if(n==9) return "IX";
-        if(n==8) return "VIII";
-        if(n==7) return "VII";
-        if(n==6) return "VI";
-        if(n==5) return "V";
-        if(n==4) return "IV";
-        if(n==3) return "III";
-        if(n==2) return "II";
-        if(n==1) return "I";
-
-        return "";
-    }
+ 
     public String intToRoman(int num) {
         StringBuilder s = new StringBuilder();
        int[] arr1 = {
