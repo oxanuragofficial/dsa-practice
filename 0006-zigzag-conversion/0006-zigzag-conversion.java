d87c4f6884102f2c93fd9d7e15d7997e1ed4166a
Anurag Kumar <hindvansianurag@gmail.com>
2026-08-20 22:11:32 +0530

    Time: 5 ms (51.9%), Space: 46.4 MB (88.93%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,24 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-        String [] ans=new String[numRows];
-        for(int i=0; i<numRows; i++){
-            ans[i]="";
+        if (numRows <= 1 || s.length() <= numRows) {
+            return s;
+        }
 
+        StringBuilder[] ans = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            ans[i] = new StringBuilder();
         }
-        int i=0; 
-        while(i<s.length()){
-            for(int index=0; index<numRows&&i<s.length(); index++){
-                ans[index]+=s.charAt(i++);
 
+        int i = 0; 
+        while (i < s.length()) {
+            for (int index = 0; index < numRows && i < s.length(); index++) {
+                ans[index].append(s.charAt(i++));
             }
-            for(int index=numRows-2; index>0&&i<s.length(); index--){
-                ans[index]+=s.charAt(i++);
+            for (int index = numRows - 2; index > 0 && i < s.length(); index--) {
+                ans[index].append(s.charAt(i++));
             }
         }
-        String res="";
-        for(String str:ans){
-            res+=str;
+
+        StringBuilder res = new StringBuilder();
+        for (int k = 0; k < ans.length; k++) {
+            res.append(ans[k]);
         }
-        return res;
+
+        return res.toString();
     }
 }
