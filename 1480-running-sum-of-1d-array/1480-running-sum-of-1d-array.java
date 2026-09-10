6628c646a57c408c2c7b6559ecc4fabcadae4146
Anurag Kumar <hindvansianurag@gmail.com>
2026-09-10 08:54:43 +0530

    Time: 0 ms (100%), Space: 44.3 MB (53.92%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -4,10 +4,10 @@ int n=nums.length;
 int [] arr=new int[n];
 arr[0]=nums[0];
         for(int i=1; i<n; i++){
-            arr[i]=arr[i-1]+nums[i];
+            nums[i]=nums[i-1]+nums[i];
         }
 
-        return arr;
+        return nums;
     }
 
 }
