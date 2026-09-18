bc45c2dac635db76c7f278a75bb87139b617d863
Anurag Kumar <hindvansianurag@gmail.com>
2026-09-18 08:33:55 +0530

    Time: 93 ms (0.84%), Space: 44.3 MB (52.02%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public boolean contains(int [] a, int n){
+        for(int i=0; i<a.length; i++){
+            if(a[i]==n){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans=new ArrayList<>();
+        Arrays.sort(target);
+        int max=0;
+        for(int i=0; i<target.length; i++){
+            max=Math.max(target[i],max);
+        }
+
+        for(int i=1; i<=n; i++){
+            if(contains(target,i)){
+                ans.add("Push");
+            }
+            else if (i<=max){
+                ans.add("Push");
+                ans.add("Pop");
+            }
+        }
+
+        return ans;
+    }
+}
